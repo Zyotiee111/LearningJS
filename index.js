e64f474124a2caccee2ead367119c820d985d7f8
Jyoti Bista <zyotieebista12@gmail.com>
2020-07-21 22:19:58 +0545

    continued

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ for (var i = 0; i<arr.length; i++){
         //break;
         continue;
     }
-    console.log(arr[i])
+    // console.log(arr[i])
 }
 
 // let j = 0;
@@ -70,3 +70,109 @@ for (var i = 0; i<arr.length; i++){
 //     console.log(arr[j]);
 //     j++;
 // }while(j<arr.length)
+
+//Array methods
+// let myArr = ["Fan", "Camera", 34, null,true];
+// console.log(myArr.length);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.push("jyoti");
+// console.log(myArr);
+
+// myArr.shift("jyoti");
+// console.log(myArr);
+
+
+// myArr.unshift("jyotie");
+// console.log(myArr);
+
+
+//string methods
+// let myStr = "jyoti is love";
+// console.log(myStr.length);
+// console.log(myStr.indexOf("love"));
+// console.log(myStr.lastIndexOf("is"));
+// console.log(myStr.slice(0,5));
+// d =myStr.replace("love","fire")
+// console.log(d, myStr)
+
+//Js dates
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getMonth);
+// console.log(myDate.getSeconds);
+
+
+//dom
+
+let elem = document.getElementById('click');
+// console.log(elem);
+
+let elemclass = document.getElementsByClassName("container");
+// console.log(elemclass);
+//elemclass[0].style.background = "yellow";
+// elemclass[0].classList.add("bg-primary");
+// elemclass[0].classList.add("text-success");
+// console.log(elemclass[0].innerHTML);
+// console.log(elemclass[1].innerText);
+
+tn = document.getElementsByTagName('div');
+console.log(tn);
+createdElement = document.createElement('p');
+createdElement.innerText = "THis is created para";
+tn[0].appendChild(createdElement);
+createdElement2 = document.createElement('b');
+createdElement2.innerText = "THis is bold";
+tn[0].replaceChild(createdElement2, createdElement);
+// removeChild(element)---> removes an elemnt
+//selecting using query
+
+// sel = document.querySelector('.container')
+// console.log(sel)
+// //events (mouse up ,mosue down)
+// function clicked(){
+//     console.log("The button was clicked");
+// }
+// window.onload = function(){
+//     console.log("the document was loaded");
+
+// firstconainer.addEventListener('click',function(){
+//     console.log("clicked on container");
+// })
+// firstconainer.addEventListener('mouseover',function(){
+//     console.log("Mouse on container");
+// })
+// firstconainer.addEventListener('mouseout',function(){
+//     console.log("Mouse out of container");
+// })
+
+// firstconainer.addEventListener('click',function(){
+//     document.querySelectorAll('.container')[1].innerHTML = "<b> we have clicked </b>"
+//     console.log("clicked on container");
+// })
+// let prevhtml =  document.querySelectorAll('.container')[1].innerHTML;
+// firstconainer.addEventListener('mouseup',function(){
+//     document.querySelectorAll('.container')[1].innerHTML = prevhtml;
+//     console.log("mouse up when clicked on container");
+// })
+// firstconainer.addEventListener('mousedown',function(){
+//     document.querySelectorAll('.container')[1].innerHTML = "<b> we have clicked </b>"
+//     console.log("mouse down when clicked on container");
+// })
+
+//set time out and set interval
+//arrow functions
+
+// function summ(a,b){
+//     return 5;
+// }
+// summ = (a,b)=>{
+//     return a+b
+// }
+logkaro = ()=>{
+    console.log("i m log")
+    document.querySelectorAll('.container')[1].innerHTML = " set interval fired"
+}
+clr = setTimeout(logkaro , 2000)
+clr = setInterval(logkaro , 2000);
